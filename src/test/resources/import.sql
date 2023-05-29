9872722b029e6145d3e82373b17a82d27d376b78
aibrahimsalem <a.ibrahim.salem@gmail.com>
2023-05-29 11:48:32 +0200

    Added test data

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -2,4 +2,4 @@ insert into customer(id, name, phone, email, address) values (1, 'ahmed', '01727
 insert into customer(id, name, phone, email, address) values (2, 'moataz', '0123456789', 'm.b@d.e', 'Leonrodplatz');
 insert into flowers_order(id, customer_id) values (2, 2);
 insert into item_type(id, name) values(2, 'flower');
-insert into item(id, name, quantity, type, price, color, material, item_type_id) values('2', 'flowerA', 10, 'plastic', 1.25, 'red', 'paper', 2);
+insert into item(id, name, quantity, type, price, color, material, item_type_id) values(2, 'flowerA', 10, 'plastic', 1.25, 'red', 'paper', 2);
